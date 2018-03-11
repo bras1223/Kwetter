d8bdb1c1a9af186e2e7f14c27b202b3fcda2b48d
bras1223 <l.hermans98@gmail.com>
2018-03-11 14:40:20 +0100

    Added possibility to delete messages.

--- service/src/main/java/nl/luukhermans/service/MessageService.java
+++ service/src/main/java/nl/luukhermans/service/MessageService.java
@@ -4,12 +4,15 @@ import nl.luukhermans.dao.JPA;
 import nl.luukhermans.dao.MessageDao;
 import nl.luukhermans.dao.UserDao;
 import nl.luukhermans.domain.Message;
-import nl.luukhermans.domain.User;
+import nl.luukhermans.domain.Trend;
+import nl.luukhermans.service.interceptor.TrendInterceptor;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.interceptor.Interceptors;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Stateless
@@ -26,18 +29,38 @@ public class MessageService {
     @JPA
     private UserDao userDao;
 
-    public void addMessage(Long id, String messageText) throws Exception {
-        Objects.requireNonNull(messageText);
-        User sender = userDao.findByID(id);
-        if (Objects.isNull(sender)) {
-            throw new Exception("User doesn't exist");
-        }
+    /**
+     * Adds a message to the MessageDao
+     * Message should not be null, so do the sender and String message.
+     *
+     * @param message The message that should be added.
+     */
+    @Interceptors(TrendInterceptor.class)
+    public void addMessage(Message message) {
+        Objects.requireNonNull(message);
+        Objects.requireNonNull(message.getMessage());
+        Objects.requireNonNull(message.getSender().getID());
 
-        Message message = Message.builder().message(messageText).sender(sender).timestamp(LocalDateTime.now()).build();
-
-        sender.addMessage(message);
+        message.setTimestamp(LocalDateTime.now());
+        List<Trend> trends = trendService.addTrendsFromMessage(message.getMessage(), true);
+        message.setTrends(trends);
         messageDao.addMessage(message);
     }
+
+    /**
+     * Deletes a message from the UserDao
+     * Message should not be null.
+     *
+     * @param message The message that should be deleted.
+     */
+    public void deleteMessage(Message message) {
+        Objects.requireNonNull(message);
+        trendService.addTrendsFromMessage(message.getMessage(), false);
+
+        messageDao.deleteMessage(message);
+    }
+
+
 
     public Collection<Message> getAllMessages() {
         return messageDao.getAllMessages();
