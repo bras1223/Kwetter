875e994e1d96538900f38d93bfa485156d4a1f20
bras1223 <l.hermans98@gmail.com>
2018-03-11 14:39:37 +0100

    Added DTO's to resources.

--- rest/src/main/java/MessageResource.java
+++ rest/src/main/java/MessageResource.java
@@ -1,12 +1,12 @@
+import dto.MessageDTO;
 import nl.luukhermans.domain.Message;
 import nl.luukhermans.service.MessageService;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.LinkedList;
 import java.util.List;
 
 @Path("messages")
@@ -17,10 +17,25 @@ public class MessageResource {
     MessageService messageService;
 
     @GET
-    @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Message> getAll() {
-        return (List) messageService.getAllMessages();
+    public List<MessageDTO> getAll() {
+        List<MessageDTO> messages = new LinkedList<MessageDTO>();
+        for (Message m : messageService.getAllMessages()) {
+            messages.add(new MessageDTO(m, true));
+        }
+
+        return messages;
     }
 
+    @POST
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Message createMessage(Message message) {
+        try {
+            messageService.addMessage(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
 }
--- rest/src/main/java/UserResource.java
+++ rest/src/main/java/UserResource.java
@@ -1,13 +1,12 @@
+import dto.UserDTO;
 import nl.luukhermans.domain.User;
 import nl.luukhermans.service.UserService;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.LinkedList;
 import java.util.List;
 
 @Path("users")
@@ -18,25 +17,70 @@ public class UserResource {
     UserService userService;
 
     @GET
-    @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getAll() {
-        return (List) userService.getAllUsers();
+    public List<UserDTO> getAll() {
+        List<UserDTO> users = new LinkedList<UserDTO>();
+        for (User u : userService.getAllUsers()) {
+            users.add(new UserDTO(u, true));
+        }
+        return users;
     }
 
     @GET
     @Path("/un={username}")
     @Produces(MediaType.APPLICATION_JSON)
-    public User getUserByID(@PathParam("username") String username) throws Exception {
-        User user = userService.findUserByUsername(username);
+    public UserDTO getUserByID(@PathParam("username") String username) throws Exception {
+        UserDTO user = new UserDTO(userService.findUserByUsername(username), true);
         return user;
     }
 
     @GET
     @Path("/id={id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public User getUserByUsername(@PathParam("id") Long ID) throws Exception {
-        User user = userService.findUserByID(ID);
+    public UserDTO getUserByUsername(@PathParam("id") Long ID) throws Exception {
+        UserDTO user = new UserDTO(userService.findUserByID(ID), true);
         return user;
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public User createUser(User user) {
+        try {
+            userService.addUser(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    @POST
+    @Path("{id}/following/{following}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public UserDTO followUser(@PathParam("id") Long ID, @PathParam("following") Long Following) {
+        User user = null;
+        try {
+            user = userService.findUserByID(ID);
+            userService.followUser(user, userService.findUserByID(Following));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new UserDTO(user, true);
+    }
+
+    @DELETE
+    @Path("{id}/following/{following}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public UserDTO unfollowUser(@PathParam("id") Long ID, @PathParam("following") Long Following) {
+        User user = null;
+        try {
+            user = userService.findUserByID(ID);
+            userService.unfollowUser(user, userService.findUserByID(Following));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new UserDTO(user, true);
     }
 }
